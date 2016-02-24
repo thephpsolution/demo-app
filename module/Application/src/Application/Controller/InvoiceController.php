8699a21e0ee26fafab8f3fc0b371575d2e0ab888
Jonathan McCartney <jonathaninthefield@gmail.com>
2016-02-23 20:15:20 -0600

    Added invoice controller.

--- module/Application/src/Application/Controller/InvoiceController.php
+++ module/Application/src/Application/Controller/InvoiceController.php
@@ -28,4 +28,85 @@ class InvoiceController extends AbstractActionController
             'form'  => $form
         ]);
     }
+
+    public function createAction()
+    {
+        $form = (new AnnotationBuilder())->createForm(Invoice::class);
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $invoice = new Invoice();
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $invoice->exchangeArray($form->getData());
+                $pdf = new InvoicePdf();
+                $pdf->bind($invoice)->flatten()->send("invoice.pdf");
+
+                return $this->redirect()->toRoute('home');
+            } else {
+                // Flash back with form errors
+            }
+        }
+        return $this->redirect()->toRoute('home');
+    }
+
+    public function em()
+    {
+        return $this->em ?: $this->getServiceLocator()->get('DoctrineORMEntityManager');
+    }
+
+    public function getMockData()
+    {
+        return array(
+            'invoicedOn' => '2012-05-05',
+            'invoiceNumber' => '123',
+            'customerId' => '123',
+            'billTo' => '321 sdf',
+            'logs' =>
+                array (
+                    0 =>
+                        array (
+                            'id' => '',
+                            'developer' => 'sdf',
+                            'description' => 'sdfg',
+                            'hourlyPrice' => '1',
+                            'hours' => '1',
+                        ),
+                    1 =>
+                        array (
+                            'id' => '',
+                            'developer' => 'sdf',
+                            'description' => 'sdfg',
+                            'hourlyPrice' => '1',
+                            'hours' => '1',
+                        ),
+                    2 =>
+                        array (
+                            'id' => '',
+                            'developer' => 'dsfg',
+                            'description' => 'dfg',
+                            'hourlyPrice' => '1',
+                            'hours' => '1',
+                        ),
+                    3 =>
+                        array (
+                            'id' => '',
+                            'developer' => 'dfrg',
+                            'description' => 'dfg',
+                            'hourlyPrice' => '1',
+                            'hours' => '1',
+                        ),
+                    4 =>
+                        array (
+                            'id' => '',
+                            'developer' => 'dfg',
+                            'description' => 'sdfg',
+                            'hourlyPrice' => '1',
+                            'hours' => '1',
+                        ),
+                ),
+            'comments' => 'dfgdfg',
+        );
+    }
 }
